006c172aeb6708b2858c90d123c025763c7e128b
sandeepraikar <snraikar@gmail.com>
2016-04-17 18:56:42 -0500

    Bootstrapping JAX_RS application using Jersey built-in class - Application

--- src/main/java/edu/sraikar/MyApp.java
+++ src/main/java/edu/sraikar/MyApp.java
@@ -4,6 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("webapi")
-public class MyApp extends Application{
+public class MyApp extends Application // bootstrapping JAX-RS application using Jersey built-in "Application class"
+{
 
 }
